087a96cda5d51f04874e7392f854d50e3e1b931b
RenzoAlessandro <renzo.sucari@ucsp.edu.pe>
2022-11-11 18:55:17 -0500

    registrar usuario v1.0

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -7,6 +7,9 @@ import {Customer} from './customer.model';
 import {CustomerService} from './customer.service';
 import {NgbdSortableHeader, SortEvent} from './sortable.directive';
 
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
@@ -15,17 +18,35 @@ import {NgbdSortableHeader, SortEvent} from './sortable.directive';
 })
 export class CustomersComponent implements OnInit {
 
-    // bread crumb items
-    breadCrumbItems: Array<{}>;
-    term: any;
+  // modal
+  editEvent: any;
+  formData: FormGroup;
+  submitted = false;
+
+  listRoles = [
+    { cNombre:1, tNombre: "Admin" },
+    { cNombre:2, tNombre: "Vendedor" },
+    { cNombre:3, tNombre: "Contador" }
+  ];
+  listSedes = [
+    { cNombre:1, tNombre: "Lima" },
+    { cNombre:2, tNombre: "Arequipa" },
+    { cNombre:3, tNombre: "Cuzco" }
+  ];
+
+  // bread crumb items
+  breadCrumbItems: Array<{}>;
+  term: any;
   
-    customers$: Observable<Customer[]>;
-    total$: Observable<number>;
+  customers$: Observable<Customer[]>;
+  total$: Observable<number>;
   
-    @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
   
 
-  constructor(public service: CustomerService) {
+  constructor(public service: CustomerService, 
+    private modalService: NgbModal, 
+    private formBuilder: FormBuilder) {
     this.customers$ = service.customers$;
     this.total$ = service.total$;
   }
@@ -45,4 +66,43 @@ export class CustomersComponent implements OnInit {
     this.service.sortColumn = column;
     this.service.sortDirection = direction;
   }
+
+
+  /**
+   * Open center modal
+   * @param centerDataModal center modal data
+   */
+     centerModal(centerDataModal: any) {
+      this.modalService.open(centerDataModal, { centered: true,windowClass:'modal-holder' });
+    }
+
+      /**
+   * Delete event
+   */
+  deleteEventData() {
+    this.editEvent.remove();
+    this.modalService.dismissAll();
+  }
+
+  /**
+   * Close event modal
+   */
+  closeEventModal() {
+    this.formData = this.formBuilder.group({
+      title: '',
+      category: '',
+    });
+    this.modalService.dismissAll();
+  }
+
+  /**
+   * Save the event
+   */
+  saveEvent() {
+    if (this.formData.valid) {
+
+    }
+    this.submitted = true;
+  }
+
 }
