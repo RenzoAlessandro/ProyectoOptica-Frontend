4d24ff43afdf895ad90a1b7303e39ea45a820900
Renzo Alessandro <renzo.sucari@ucsp.edu.pe>
2022-11-18 20:56:02 -0500

    formulario de add monturas

--- src/app/pages/products/add-product/add-product.component.ts
+++ src/app/pages/products/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -6,12 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
+
+  formMonturas: FormGroup;
+  submitted = false;
+  material: string = "campoMaterial";
+  marca: string = "campoMarca";
+  talla: string = "campoTalla";
+  codigo: string = "campoCodigo";
+  codigo_interno: string = "campoInterno";
+  cantidad: string = "campoCantidad";
+  precio_compra: string = "campoCompra";
+  precio_venta: string = "campoVenta";
+  fecha_registro: string = "campoFechaRegistro";
+  fecha_modificacion: string = "campoFechaModificacion";
+
   // bread crumb items
   breadCrumbItems: Array<{}>;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.crearFormulario();
+  }
+
+  crearFormulario() {
+    this.formMonturas = this.fb.group({
+      [this.material]:[],
+      [this.marca]:[],
+      [this.talla]:[],
+      [this.codigo]:[],
+      [this.codigo_interno]:[],
+      [this.cantidad]:[],
+      [this.precio_compra]:[],
+      [this.precio_venta]:[],
+      [this.fecha_registro]:[],
+      [this.fecha_modificacion]:[],
+    })
+  }
+
+  /**
+   * Save the event
+   */
+  saveEvent() {
+    if (this.formMonturas.valid) {
+
+    }
+    this.submitted = true;
   }
 
 }
