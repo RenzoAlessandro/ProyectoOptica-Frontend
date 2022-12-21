28c191cec7aa6dc696766381f4bc09b71c9586c0
eyvind1 <eyvind.osco@ucsp.edu.pe>
2022-12-21 07:38:09 -0500

    ip produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
     measurementId: ''
   },
   //urlBackend:'http://localhost:5000/' 
-  urlBackend:'http://3.83.29.81:5000/'
+  urlBackend:'35.173.191.188:5000/'
 };
 
 /*
