d3145f9ce41f184cbc50ebe3c0989922eec44294
eyvind1 <eyvind.osco@ucsp.edu.pe>
2023-01-14 07:57:19 -0500

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   year: number = new Date().getFullYear();
+  formLogin: FormGroup;
+  user: string = "campoUser";
+  password: string = "campoPassword";
+  sede: string = "campoSede";
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
-  constructor() { }
 
   ngOnInit(): void {
     document.body.classList.add('authentication-bg')
     document.body.removeAttribute('data-topbar');
+    this.crearFormulario();
+  }
+
+  f(campo: string) {
+    return this.formLogin.get(campo);
+  }
+  crearFormulario() {
+    this.formLogin = this.fb.group({
+      [this.user] :[],
+      [this.password]: []
+    })
+  }
+
+  conectar() {
+    if (this.formLogin.valid) {
+      const  vendedor = {
+        usuario: this.f(this.user).value,
+        pass: this.f(this.password).value,
+        sede: this.f('sede').value
+      }
+    } else {
+      
+    }
   }
 }
