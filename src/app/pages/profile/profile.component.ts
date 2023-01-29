76113d4aa50ace40cadfcac26f33a38345c10af4
Renzo Alessandro <renzo.sucari@ucsp.edu.pe>
2023-01-28 19:52:23 -0500

    Modal de Cambio de Contraseña

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -9,10 +11,26 @@ export class ProfileComponent implements OnInit {
   // bread crumb items
   breadCrumbItems: Array<{}>;
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Contacto Visual' }, { label: 'Perfil', active: true }];
   }
 
+  /**
+   * Open center modal
+   * @param centerDataModalChangePassword center modal data
+   */
+  centerModalChangePassword(centerDataModalChangePassword: any) {
+    this.modalService.open(centerDataModalChangePassword, { centered: true, windowClass: 'modal-holder' });
+  }
+
+  /**
+   * Close event modal
+   */
+    closeEventModal() {
+      this.modalService.dismissAll();
+    }
 }
