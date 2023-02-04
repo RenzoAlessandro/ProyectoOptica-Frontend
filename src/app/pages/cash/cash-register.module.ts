b5282ad75a4c3ee7e67c8afa63e46642e1cf2b7f
Renzo Alessandro <renzo.sucari@ucsp.edu.pe>
2023-02-03 23:29:04 -0500

    Lista en Caja

--- src/app/pages/cash/cash-register.module.ts
+++ src/app/pages/cash/cash-register.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { CashRegisterRoutingModule } from './cash-register-routing.module';
 import { UIModule } from '../../shared/ui/ui.module';
-import { NgbModule, NgbNavModule, NgbDropdownModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { Ng2FlatpickrModule } from 'ng2-flatpickr';
 import { FormModule } from '../form/form.module';
+import { NgbAccordionModule, NgbCollapseModule, NgbDropdownModule, NgbPaginationModule, NgbTooltipModule, NgbTypeaheadModule } from "@ng-bootstrap/ng-bootstrap";
+import { Ng5SliderModule } from "ng5-slider";
+import { DropzoneModule, DROPZONE_CONFIG } from "ngx-dropzone-wrapper";
+import { config } from "rxjs";
+
 import { CashRegisterComponent } from './cash-register/cash-register.component';
+import { ListCashComponent } from './list-cash/list-cash.component';
+
 
 @NgModule({
-  declarations: [CashRegisterComponent],
+  declarations: [CashRegisterComponent, ListCashComponent],
   imports: [
     CommonModule,
     FormModule,
@@ -22,14 +25,23 @@ import { CashRegisterComponent } from './cash-register/cash-register.component';
     ReactiveFormsModule,
     CashRegisterRoutingModule,
     UIModule,
+    DropzoneModule,
     NgbTooltipModule,
-    NgbNavModule,
     NgbDropdownModule,
     NgbPaginationModule,
     NgSelectModule,
     Ng2SearchPipeModule,
-    NgbModule,
-    Ng2FlatpickrModule
+    Ng2FlatpickrModule,
+    Ng5SliderModule,
+    NgbAccordionModule,
+    NgbTypeaheadModule,
+    NgbCollapseModule
+  ],
+  providers: [
+    {
+      provide: DROPZONE_CONFIG,
+      useValue: config
+    }
   ]
 })
 export class CashRegisterModule { }
