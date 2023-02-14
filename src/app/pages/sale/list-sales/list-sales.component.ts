85dfe3481f31d8ed427abe8024073b9941ad27f9
eyvind1 <eyvind.osco@ucsp.edu.pe>
2023-02-13 21:22:42 -0500

    role

--- src/app/pages/sale/list-sales/list-sales.component.ts
+++ src/app/pages/sale/list-sales/list-sales.component.ts
@@ -35,6 +35,7 @@ import { SedesModel } from 'src/models/sedes';
 
 export class ListSalesComponent implements OnInit {
 
+  role = 'Admin'
   formSedes: FormGroup;
   listSedes: Array<SedesModel>;
   nombre_sedes: string = "campoSede";
